05ef082ac1b4a90fdd6b8d7a1381a547c1377b88
efi <efi@cun.edu.co>
2026-08-12 12:16:11 -0500

    segundo entrega

--- iniciar_servidor.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-title Servidor Local CUN SST
-echo Iniciando servidor local para el Curso de Reinduccion SST...
-powershell -NoProfile -ExecutionPolicy Bypass -File "%~dp0iniciar_servidor.ps1"
-pause
